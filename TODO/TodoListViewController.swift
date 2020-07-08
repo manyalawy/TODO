894ff8499dfc2eefaccd3af022770b38f52cf1c7
Yossef El Manyalawy <manyalawy@gmail.com>
2020-07-08 23:58:29 +0200

    TODO DATA SOURCE

---
+++ TODO/TodoListViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  TODO
+//
+//  Created by Youssef El Manyalawy on 7/4/20.
+//  Copyright © 2020 Youssef El Manyalawy. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+
+    let items = ["ab" , "cf"]
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return items.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        cell.textLabel?.text  = items[indexPath.row]
+        return cell
+    }
+    
+    //MARK -  Table view delegate methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        
+        
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else{
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath, animated: true)
+        
+    }
+}
+
